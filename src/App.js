57b3c18c2c4afeedde20b86ced81a594aa0b6796
Matthew Hagel <matthew.hagel@imaginecurve.com>
2022-04-20 23:59:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>hello world</title>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          adfasfsaffasfsf what
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React! or don'ts
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
